28ccc4408dcdd33aec7bae18f85f3f0b639ebfe5
Andrew Back <andrew@carrierdetect.com>
2019-01-05 17:14:11 +0000

    Update ikiwiki-setup.sh

--- script/ikiwiki-setup.sh
+++ script/ikiwiki-setup.sh
@@ -16,6 +16,15 @@ ikiwiki --changesetup wiki.setup \
 	--adminuser admin \
 	--plugin websetup \
 	--plugin 404 \
+	--plugin goodstuff
+	--plugin favicon
+	--plugin theme
+	--plugin typography
+	--plugin img
+	--plugin sidebar
+	--plugin sparkline
+	--plugin postsparkline
+	--plugin remove
 	--set cgi_wrapper=$DESK/ikiwiki.cgi
 
 ikiwiki --setup wiki.setup
